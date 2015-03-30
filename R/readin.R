3e76fb35cf4216d1f975d3c11fb9c668f6bfe4fa
Jacob Carey <jacobcvt12@gmail.com>
2015-03-30 12:38:15 -0400

    Include horizontal data in readin

--- R/readin.R
+++ R/readin.R
@@ -1,29 +1,41 @@
-# read in data
-hivvert <- read.csv("data/hivvert-data04.csv") 
-
-# missing data
-missing <- apply(hivvert, 2, function(x) sum(is.na(x))/length(x))
-
-# data missing more than 5%
-sort(missing[missing > 0.05], decreasing=TRUE)
-
-# mean visits
+# libraries
 library(dplyr)
-visits <- hivvert %>%
-    group_by(STUDYID) %>%
-    summarise(visits=n())
-
-mean(visits$visits)
-
 library(ggplot2)
 
-# plot histogram of visits
-qplot(x=visits, data=visits)
+# read in data
+hivvert <- read.csv("data/hivvert-data04.csv") %>%
+    tbl_df()
 
-# last visit year
-library(lubridate)
-year.info <- hivvert %>%
-    group_by(STUDYID) %>%
-    summarise(last.year=max(year(mdy(VDATE))))
+hivhoriz <- read.csv("data/macswihs_horiz2013_copy-1.csv") %>%
+    tbl_df() %>%
+    # convert SAS numeric to date format
+    mutate(sero.date=as.Date("1960-01-01") + FPOSDATE,
+           sero.pos=sero.date < as.Date("2100-01-01")) %>%
+    select(STUDYID, sero.date, sero.pos, AIDSDIAG)
 
-qplot(x=last.year, data=year.info)
+# join datasets
+hiv.full <- left_join(hivvert, hivhoriz, 
+                      by="STUDYID") %>%
+    # only keep MACS data
+    filter(COHORT==1) %>%
+    # transform visit number
+    mutate(visit.num=VISIT/10) %>%
+    # keep relevant fields
+    select(id=STUDYID,
+           visit.num,
+           visit.date=VDATE,
+           cd4.count=CD4N,
+           cd4.pct=CD4P,
+           therapy=THRPY,
+           depression.score=CESD,
+           idu=IDU,
+           pot=POTUSE,
+           crack=CRACKUSE,
+           cocaine=COCUSE,
+           heroin=HERUSE,
+           amphetamine=AMPHUSE,
+           poppers=POPUSE,
+           sero.date,
+           sero.pos,
+           defining.diag=AIDSDIAG)
+
